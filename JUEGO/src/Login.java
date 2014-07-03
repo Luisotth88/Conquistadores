6f8b293bcf7a4586fbd5d5a521185e16cc25d823
DiegoC13 <d.cifuentes05@ufromail.cl>
2014-07-02 23:27:47 -0400

    ise una wea

--- JUEGO/src/Login.java
+++ JUEGO/src/Login.java
@@ -1,5 +1,12 @@
 
-import javax.swing.JTextField;
+import java.io.BufferedInputStream;
+import java.io.DataInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.swing.*;
 
 /*
  * To change this template, choose Tools | Templates
@@ -12,7 +19,7 @@ import javax.swing.JTextField;
  */
 public class Login extends javax.swing.JFrame {
 
-    guardar g;
+ 
     WindowManager wm;
     public Login(WindowManager wm) {
         this.wm = wm;
@@ -97,7 +104,7 @@ public class Login extends javax.swing.JFrame {
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
         jugador j=new jugador(jTextField1.getText(),1000);
-        g.escribir(j);
+        escribir(j);
         wm.mapa.setVisible(true);
         this.setVisible(false);
     }//GEN-LAST:event_jButton1ActionPerformed
@@ -111,7 +118,62 @@ public class Login extends javax.swing.JFrame {
     public JTextField getjTextField1() {
         return jTextField1;
     }
-  
+  public jugador leer(){
+         
+         File file = new File("save.txt");
+         FileInputStream fis = null;
+         BufferedInputStream bis = null;
+         DataInputStream dis = null;
+         String split=";";
+         String[] linea=new String[20];
+         jugador d=null;
+         try {
+            fis = new FileInputStream(file);
+            bis = new BufferedInputStream(fis);
+            dis = new DataInputStream(bis);
+         
+                        
+                
+            while (dis.available() != 0) {
+                linea = dis.readLine().split(split);}
+                d=new jugador(linea[0],Long.parseLong(linea[1]),Boolean.parseBoolean(linea[2]),Boolean.parseBoolean(linea[3]),
+                Boolean.parseBoolean(linea[4]),Boolean.parseBoolean(linea[5]),Boolean.parseBoolean(linea[6]),Boolean.parseBoolean(linea[7]),
+                  Boolean.parseBoolean(linea[8]),Boolean.parseBoolean(linea[9]),Boolean.parseBoolean(linea[10]));
+                        
+                
+                                              
+           }catch (IOException e) {
+			e.printStackTrace();
+    
+    
+             }
+           return d;
+    }
+    public void escribir(jugador d){
+        FileWriter fichero = null;
+        PrintWriter pw = null;
+        try
+        {
+            fichero = new FileWriter("save.txt");
+            pw = new PrintWriter(fichero);
+ 
+            
+                pw.println(""+d.getNombre()+";"+""+d.getPuntaje()+";"+""+d.isC1()+";"+""+d.isC2()+";"+""+d.isC3()+";"+""+d.isC4()+";"+""+d.isC5()
+                +";"+""+d.isC6()+";"+""+d.isC7()+";"+""+d.isC8()+";"+""+d.isC9());
+ 
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+           try {
+           // Nuevamente aprovechamos el finally para 
+           // asegurarnos que se cierra el fichero.
+           if (null != fichero)
+              fichero.close();
+           } catch (Exception e2) {
+              e2.printStackTrace();
+           }
+        }
+    }
     
     
     
